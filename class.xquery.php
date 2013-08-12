9341578ed60a1322567f512fa941e4abc3266bac
romain perez <perez.romain@gmail.com>
2013-08-12 19:30:32 +0200

    doc

--- class.xquery.php
+++ class.xquery.php
@@ -1,82 +1,191 @@
 <?php
+/**
+ * @author rohm1
+ */
 
+/**
+ * Shortcut function to create a new xQuery object
+ */
 function xQuery($doc) {
 	return new xQuery($doc);
 }
 
+/**
+ * xQuery
+ */
 class xQuery {
 
+	/**
+	 *
+	 *
+	 * @var array
+	 */
 	public static $ATTRS = array('#' => 'id', '.' => 'class');
 
+	/**
+	 * The document
+	 *
+	 * @var DOMDocument
+	 */
 	private $doc;
 
-	public function __construct($doc) {
-		if(is_string($doc)) {
-			$this->doc = $this->createDOMDocument();
-			if(preg_match('#^<([a-zA-Z]+)#', trim($doc))) {
-				@$this->doc->loadHTML($doc);
-			}
-			else {
-				@$this->doc->loadHTMLFile($doc);
-			}
+	/**
+	 * CSS selector used to create this object
+	 *
+	 * @var string
+	 */
+	private $selector = '';
 
-		}
-		else {
-			$this->doc = $this->createDOMDocument($doc);
-		}
+	/**
+	 * Parent document
+	 *
+	 * @var xQuery
+	 */
+	private $prev = null;
+
+	/**
+	 * Number of child nodes
+	 *
+	 * @var int
+	 */
+	public $length;
+
+	/**
+	 * Constructor
+	 *
+	 * @param mixed $doc The document to manipulate.
+	 * 	$doc can be any of (X)HTML string, a (X)HTML document location,
+	 * 	a DOMDocument, a DOMElement, or a DOMNodeList.
+	 * @param string $selector The selector used to create this instance.
+	 * 	You should never set this.
+	 * @param xQuery $prev The parent xQuery instance of this document.
+	 * 	You should never set this.
+	 */
+	public function __construct($doc, $selector = '', xQuery $prev = null) {
+		$this->createDOMDocument($doc);
+
+		$this->length = $this->doc->childNodes->length;
+		$this->selector = $selector;
+		$this->prev = $prev;
 	}
 
+	/**
+	 * Creates the DOMDocument
+	 *
+	 * @param mixed $doc
+	 * @return void
+	 */
+	private function createDOMDocument($doc) {
+		$_doc = new DOMDocument();
+		$_doc->validateOnParse = true;
+		$_doc->preserveWhiteSpace = false;
+		$_doc->strictErrorChecking = false;
 
-	private function createDOMDocument($nodes = '') {
-		$doc = new DOMDocument();
-		$doc->validateOnParse = true;
-		$doc->preserveWhiteSpace = false;
-		$doc->strictErrorChecking = false;
-
-		if($nodes != '') {
-			@$doc->loadHTML('<root></root>');
-			if(get_class($nodes) == 'DOMElement') {
-				$doc->documentElement->appendChild( $doc->importNode($nodes, true) );
+		if (is_string($doc)) {
+			if (preg_match('#^<([a-zA-Z]+)#', trim($doc))) {
+				@$_doc->loadHTML($doc);
 			}
 			else {
-				foreach($nodes as $node) {
-					$doc->documentElement->appendChild( $doc->importNode($node, true) );
+				@$_doc->loadHTMLFile($doc);
+			}
+		}
+		else {
+			$class = get_class($doc);
+			if ($class == 'DOMElement') {
+				$_doc->appendChild( $_doc->importNode($doc, true) );
+			}
+			else if (in_array($class, ['DOMDocument', 'DOMNodeList'])) {
+				foreach ($doc as $node) {
+					$_doc->appendChild( $_doc->importNode($node, true) );
 				}
 			}
 		}
 
-		return $doc;
+		$this->doc = $_doc;
 	}
 
-	public function get($css = '', $index = -1) {
-		$xpath = self::parse($css);
+	/**
+	 * Queries the DOM with a CSS selector
+	 *
+	 * @param string $selector
+	 * @param int $index
+	 * @return xQuery
+	 */
+	public function find($selector, $index = -1) {
+		$xpath = self::parse($selector);
 
 		$dxpath = new DOMXPath($this->doc);
+		$res = $dxpath->query($xpath);
+		if ($index != -1) {
+			$res = $res->item($index);
+		}
 
-		if ($index == -1) {
-			return new xQuery($dxpath->query($xpath));
-		}
-		else {
-			return new xQuery($dxpath->query($xpath)->item($index));
-		}
+		return self::mkRes($res, $selector);
 	}
 
+	/**
+	 * Selects the $index node in the document
+	 *
+	 * @param int $index
+	 * @return xQuery
+	 */
+	public function eq($index) {
+		return self::mkRes($this->doc->childNodes->item($index), $this->selector);
+	}
+
+	/**
+	 * Returns the HTML version of the document
+	 *
+	 * @return string
+	 */
 	public function html() {
-		return $this->doc->textContent;
+		return $this->doc->saveHtml();
 	}
 
-	private function parse($css) {
-		$css = trim($css);
-		$l = strlen($css);
+	/**
+	 * Returns the parent node of the document
+	 *
+	 * Note: This requires the current document is the result of
+	 * a xQuery->find()
+	 *
+	 * @return xQuery
+	 */
+	public function parent() {
+		//TODO implement
+	}
+
+	/**
+	 * Returns a parent node of the document filtered by a selector
+	 *
+	 * Note: This requires the current document is the result of
+	 * a xQuery->find()
+	 *
+	 * @param string $selector
+	 * @return xQuery
+	 */
+	public function parents($selector) {
+		$xpath = self::parse($selector);
+
+		//TODO implement
+	}
+
+	/**
+	 * Parses a CSS selector
+	 *
+	 * @return string
+	 */
+	private function parse($selector) {
+		$selector = trim($selector);
+		$l = strlen($selector);
 		$i = 0;
-		$xpath = '';
 		$rules = [];
 		$crt_rule = ['direct_child' => false];
 
 		while ($i < $l) {
-			if (array_key_exists(substr($css, $i, 1), self::$ATTRS)) {
-				$type = self::$ATTRS[substr($css, $i, 1)];
-				$value = self::extract_class_or_id($css, $l, $i);
+			$crt_char = substr($selector, $i, 1);
+			if (array_key_exists($crt_char, self::$ATTRS)) {
+				$type = self::$ATTRS[$crt_char];
+				$value = self::extractClassOrId($selector, $l, $i);
 
 				if ($type == 'class') {
 					if (!isset($crt_rule['class'])) {
@@ -88,47 +197,74 @@ class xQuery {
 					$crt_rule[$type] = $value;
 				}
 			}
-			elseif (preg_match('/[a-zA-Z]/', substr($css, $i, 1))) {
-				$crt_rule['tagName'] = self::extract_tag_name($css, $l, $i);
+			elseif (preg_match('/[a-zA-Z]/', $crt_char)) {
+				$crt_rule['tagName'] = self::extractTagName($selector, $l, $i);
+			}
+			elseif ($crt_char == '*') {
+				$crt_rule['tagName'] = '*';
+				$i++;
 			}
 			else {
 				$rules[] = $crt_rule;
-				$crt_rule = [];
-				$crt_rule['direct_child'] = self::go_to_next($css, $l, $i);
+				$crt_rule = ['direct_child' => self::findNextRule($selector, $l, $i)];
 			}
 		}
 		$rules[] = $crt_rule;
 
-		return self::rules_to_xpath($rules);
+		return self::rules2Xpath($rules);
 	}
 
-	private function extract_class_or_id($css, $l, &$offset) {
+	/**
+	 * Extracts a class or ID name
+	 *
+	 * @param string $selector
+	 * @param int $l
+	 * @param int $offset
+	 * @return string
+	 */
+	private function extractClassOrId($selector, $l, &$offset) {
 		$i = 1;
-		while ($i < $l && preg_match('/[a-zA-Z0-9_-]/', substr($css, $offset + $i, 1))) {
+		while ($i < $l && preg_match('/[a-zA-Z0-9_-]/', substr($selector, $offset + $i, 1))) {
 			$i++;
 		}
 
-		$attr = substr($css, $offset + 1, $i - 1);
+		$attr = substr($selector, $offset + 1, $i - 1);
 		$offset += $i;
 		return $attr;
 	}
 
-	private function extract_tag_name($css, $l, &$offset) {
+	/**
+	 * Extracts a tag name
+	 *
+	 * @param string $selector
+	 * @param int $l
+	 * @param int $offset
+	 * @return string
+	 */
+	private function extractTagName($selector, $l, &$offset) {
 		$i = 1;
-		while ($i < $l && preg_match('/[a-zA-Z]/', substr($css, $offset + $i, 1))) {
+		while ($i < $l && preg_match('/[a-zA-Z]/', substr($selector, $offset + $i, 1))) {
 			$i++;
 		}
 
-		$attr = substr($css, $offset, $i);
+		$attr = substr($selector, $offset, $i);
 		$offset += $i;
 		return $attr;
 	}
 
-	private function go_to_next($css, $l, &$offset) {
+	/**
+	 * Strips white spaces to the next CSS rule
+	 *
+	 * @param string $selector
+	 * @param int $l
+	 * @param int $offset
+	 * @return bool is the new selector a direct child?
+	 */
+	private function findNextRule($selector, $l, &$offset) {
 		$is_direct_child = false;
 		$i = 0;
-		while ($i < $l && (substr($css, $offset + $i, 1) == ' ' || (!$is_direct_child && substr($css, $offset + $i, 1) == '>'))) {
-			if (!$is_direct_child && substr($css, $offset + $i, 1) == '>') {
+		while ($i < $l && (substr($selector, $offset + $i, 1) == ' ' || (!$is_direct_child && substr($selector, $offset + $i, 1) == '>'))) {
+			if (!$is_direct_child && substr($selector, $offset + $i, 1) == '>') {
 				$is_direct_child = true;
 			}
 			$i++;
@@ -138,35 +274,51 @@ class xQuery {
 		return $is_direct_child;
 	}
 
-	private function rules_to_xpath($rules) {
+	/**
+	 * Translates the set of rules in a CSS selector into a xpath expression
+	 *
+	 * @param array $rules
+	 * @return string
+	 */
+	private function rules2Xpath($rules) {
 		$xpath = '';
 
 		foreach ($rules as $rule) {
+			$properties = [];
+
 			if ($rule['direct_child']) {$xpath .= '/';}
 			else                       {$xpath .= '//';}
 
 			if (isset($rule['tagName'])) {$xpath .= $rule['tagName'];}
+			else                         {$xpath .= '*';}
 
 			if (isset($rule['class'])) {
-				if (!isset($rule['tagName'])) {
-					$xpath .= '*';
-				}
-				$classes = [];
 				foreach ($rule['class'] as $class) {
-					$classes[] = 'contains(concat(" ", normalize-space(@class), " "), concat(" ", "' . $class . '", " "))';
+					$properties[] = 'contains(concat(" ", @class, " "), concat(" ", "' . $class . '", " "))';
 				}
-				$xpath .= '[' . implode(' and ', $classes) . ']';
 			}
 
 			if (isset($rule['id'])) {
-				if (!isset($rule['tagName'])) {
-					$xpath .= '*';
-				}
-				$xpath .= '[@id="' . $rule['id'] . '"]';
+				$properties[] = '@id="' . $rule['id'] . '"';
+			}
+
+			if (count($properties) > 0) {
+				$xpath .= '[' . implode(' and ', $properties) . ']';
 			}
 		}
 
 		return $xpath;
+	}
+
+	/**
+	 * Creates the object to return
+	 *
+	 * @param DOMNodeList $res
+	 * @param string $selector
+	 * @return xQuery
+	 */
+	private function mkRes($res, $selector) {
+		return new xQuery($res, $selector, $this);
 	}
 
 }
